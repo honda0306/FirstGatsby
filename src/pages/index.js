3146c1793945c413561a99cca4fa129a203b598b
Chris Honda <christopher.honda92@gmail.com>
2018-11-22 22:55:27 -0800

    Testing some content and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,12 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+    <h1>Hey everyone, I have an announcement:</h1>
+    <h3>This is my first <i>real</i> Gatsby site.</h3>
+    <p>I know, I know... I'm a bit late to the game, but I'm still very excited to learn about it!</p>
+    {/* <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
-    </div>
+    </div> */}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>This is embarassing, but...</h1>
+    <h3>I don't have anything to share here yet!</h3>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
